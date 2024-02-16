2a2544c14e79764a9e9372ad9cdb295e9f979e3c
Felipe Lussi <felipe-p-l@hotmail.com>
2024-02-16 11:39:46 -0300

    Feat: Create extract function

--- Banks_Project/banks_ETL.py
+++ Banks_Project/banks_ETL.py
@@ -18,3 +18,26 @@ def log_progress(message):
     with open('code_log.txt', 'a') as log:
         log.write(now + " " + message + "\n")
         
+def extract(url):
+    page = requests.get(url).text
+    
+    soup = BeautifulSoup(page, 'html.parser')
+    
+    df = pd.DataFrame(columns=['Name', 'MC_USD_Billion'])    
+    table = soup.find_all('tbody')[0]
+    rows = table.find_all('tr')
+    
+    for row in rows:
+        col = row.find_all('td')
+        if len(col) != 0 :
+             
+            data_dict = {'Name': col[1].find_all('a')[1],
+                         'MC_USD_Billion': str(col[2].contents[0]).split('\n')[0]}
+            
+            df1 = pd.DataFrame(data_dict, index=[0])
+            df = pd.concat([df, df1], ignore_index=True)
+    
+    return df
+
+print(extract(data_url))
+    
