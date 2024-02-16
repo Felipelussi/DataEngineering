ddee8e034a3b1fca752b06b25958a36c35ee9b04
Felipe Lussi <felipe-p-l@hotmail.com>
2024-02-16 11:11:51 -0300

    create log_progress function

--- Banks_Project/banks_ETL.py
+++ Banks_Project/banks_ETL.py
@@ -6,3 +6,15 @@ import sqlite3
 import datetime
 
 
+data_url = 'https://web.archive.org/web/20230908091635 /https://en.wikipedia.org/wiki/List_of_largest_banks'
+
+
+
+def log_progress(message):
+    timestamp = '%Y-%m-%d %h-%m-%s'
+    now = datetime.datetime.now()
+    now = now.strftime(timestamp)
+    
+    with open('code_log.txt', 'a') as log:
+        log.write(now + " " + message + "\n")
+        
