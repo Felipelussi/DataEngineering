1363f977cff9700f3b851b0d31853932a027b9a2
Felipe Lussi <felipe-p-l@hotmail.com>
2024-04-30 07:56:52 -0300

    feat: add transform function

--- Banks_Project/banks_ETL.py
+++ Banks_Project/banks_ETL.py
@@ -6,24 +6,20 @@ import sqlite3
 import datetime
 
 
-data_url = 'https://web.archive.org/web/20230908091635 /https://en.wikipedia.org/wiki/List_of_largest_banks'
-
-
-
 def log_progress(message):
-    timestamp = '%Y-%m-%d %h-%m-%s'
+    timestamp = '%Y-%m-%d %H-%M-%S'
     now = datetime.datetime.now()
     now = now.strftime(timestamp)
     
-    with open('code_log.txt', 'a') as log:
-        log.write(now + " " + message + "\n")
+    with open(log_file, 'a') as log:
+        log.write(now + ' ' + message + '\n')
         
-def extract(url):
+def extract(url, table_attribs):
     page = requests.get(url).text
     
     soup = BeautifulSoup(page, 'html.parser')
     
-    df = pd.DataFrame(columns=['Name', 'MC_USD_Billion'])    
+    df = pd.DataFrame(columns=table_attribs)    
     table = soup.find_all('tbody')[0]
     rows = table.find_all('tr')
     
@@ -32,12 +28,20 @@ def extract(url):
         if len(col) != 0 :
              
             data_dict = {'Name': col[1].find_all('a')[1],
-                         'MC_USD_Billion': str(col[2].contents[0]).split('\n')[0]}
+                         'MC_USD_Billion': float(col[2].contents[0][:-1])}
             
             df1 = pd.DataFrame(data_dict, index=[0])
             df = pd.concat([df, df1], ignore_index=True)
     
     return df
 
-print(extract(data_url))
+def transform(df, csv_path):
+    exchange_rates = pd.read_csv(csv_path, index_col=0)  
     
+   #dict = exchange_rates.set_index('Currency').to_dict()['Rate']
+    
+    df['MC_GBP_Billion'] = [np.round(x*(exchange_rates['Rate']['GBP']), 2) for x in df['MC_USD_Billion']]
+    df['MC_EUR_Billion'] = [np.round(x*(exchange_rates['Rate']['EUR']), 2) for x in df['MC_USD_Billion']]
+    df['MC_INR_Billion'] = [np.round(x*(exchange_rates['Rate']['INR']), 2) for x in df['MC_USD_Billion']]
+
+    return df
